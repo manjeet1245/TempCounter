6385ee3f0df5dd51e501149612bfd35a8c6e053a
Manjeet Singh <manjeets1245@gmail.com>
2023-03-30 01:50:59 +0530

    finalCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count,setCount] = useState(0);
+
+  // function decrState(){
+  //   setCount( count-1);
+  // }
+
+  // function incrState(){
+  //   setCount( count+1);
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='app'>{count}</h1>
+      <button className='incrBtn' onClick={() => setCount(count+1)}>INCR</button>
+      <button className='decrBtn' onClick={() => setCount(count-1)}>DECR</button>
     </div>
   );
 }
