a333feafa96fa932b7fe4149eae7cfb09ed36a94
Manjeet Singh <manjeets1245@gmail.com>
2023-03-30 12:51:57 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 
+import { useEffect } from 'react';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  const [count,setCount] = useState(0);
+  const [count,setCount] = useState(-1);
 
-  // function decrState(){
-  //   setCount( count-1);
-  // }
+  function decrState(){
+    // return(
+      setCount( count-1)
+      
+  }
 
-  // function incrState(){
-  //   setCount( count+1);
-  // }
+  function incrState(){
+    // return(
+      setCount( count+1)
+      
+  }
+  
   return (
     <div className="App">
       <h1 className='app'>{count}</h1>
-      <button className='incrBtn' onClick={() => setCount(count+1)}>INCR</button>
-      <button className='decrBtn' onClick={() => setCount(count-1)}>DECR</button>
+      <button className='incrBtn' onClick={ incrState}>INCR</button>
+      <button className='decrBtn' onClick={decrState}>DECR</button>
     </div>
   );
 }
